9044d7feb55790c3ace3f4972fbc3624b1ec78ee
joeslzr <38015581+joeslzr@users.noreply.github.com>
2020-02-11 13:02:48 -0700

    Update scripts.js

--- SENG513/A2/scripts.js
+++ SENG513/A2/scripts.js
@@ -1,10 +1,9 @@
 var ANS = 0;
 
-
 function append(input){
 
     let displayStr = document.getElementById("display").value
-
+    
     if((displayStr == 0 && !isNaN(input) && displayStr.substr(displayStr.length - 1) != '.') // if blank and inputting a new number overwrite the 0
         || (displayStr == 0 && input == '(')
         || (displayStr == 0 && input == ')') 
@@ -20,8 +19,6 @@ function append(input){
     }else if(!isNaN(input) && displayStr.substr(displayStr.length - 3) == "ANS"){
         document.getElementById("display").value += '×' + input; 
         console.log(2);
-    }else if(displayStr.includes('.') && input == '.'){
-        ;
     }else{
         console.log(4);
         document.getElementById("display").value += input;
@@ -65,14 +62,28 @@ function eql(){
 
     let displayStr = document.getElementById("display").value;
 
+    if(displayStr.includes('=') || displayStr == 0){
+        document.getElementById("display").value = "0";
+        return; 
+    }
+
+
     displayStr = displayStr.replace(/×/gi, "*");
     displayStr = displayStr.replace(/÷/gi, "/");
     displayStr = displayStr.replace("ANS", ANS);
 
+
+
+
     try{
         let result = eval(displayStr);
 
-        ANS = result;
+        console.log(result);
+        if(result== "" || result == undefined){
+            let result = "Syntax Error";
+        }else{
+            ANS = result;
+        }
         document.getElementById("display").value += '=' + result;
 
     } catch (e) {
